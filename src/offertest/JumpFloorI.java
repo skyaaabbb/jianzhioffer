4b624214d77aebb7fd647ba6f0cfd6b1bacbfe25
yixiaobai <yixiaobai123@gmail.com>
2020-06-04 21:48:17 +0800

    青蛙跳台阶

--- src/offertest/JumpFloorI.java
+++ src/offertest/JumpFloorI.java
@@ -26,17 +26,17 @@ public class JumpFloorI {
         }
         //实现方法二 循环
     public static int JumpFloor2(int target){
-            if(target <= 0) return 0;
-            if(target == 1) return 1;
-            if(target == 2) return 2;
-            int first = 1;
-            int second = 2;
-            int result = 0;
-            for(int i = 3; i <= target;i++){
-                result = first + second;
-                first = second;
-                second = result;
-            }
-            return result;
+        if (target <= 0) return 0;
+        if (target == 1)return 1;
+        if (target == 2)return 2;
+        int result = 0;
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= target; i++){
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return result;
     }
 }
